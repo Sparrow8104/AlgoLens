27feef02a5a8e4fef44f223095bf13bf355a1217
Sparrow8104 <anchalsingh8102004@gmail.com>
2026-03-26 11:17:56 +0530

    A Codeforces Session Manager to manage sessions on Codeforces

---
+++ src/main/java/com/algolens/algo_lens/config/CfSessionManager.java
@@ -0,0 +1,87 @@
+package com.algolens.algo_lens.config;
+
+import com.algolens.algo_lens.exception.CfAuthException;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Component
+public class CfSessionManager {
+
+    @Value("${codeforces.handle}")
+    private String handle;
+
+    @Value("${codeforces.password}")
+    private String password;
+
+    private String sessionCookie;
+    private LocalDateTime lastLogin;
+
+    public String getSession() {
+        if (sessionCookie == null || lastLogin == null
+                || lastLogin.isBefore(LocalDateTime.now().minusHours(6))) {
+            login();
+        }
+        return sessionCookie;
+    }
+
+    private void login(){
+        try{
+            Connection.Response loginPage = Jsoup.connect(
+                            "https://codeforces.com/enter")
+                    .userAgent("Mozilla/5.0 (compatible; AlgoLens/1.0)")
+                    .method(Connection.Method.GET)
+                    .execute();
+
+            String csrf=loginPage.parse()
+                    .select("input[name=csrf_token]")
+                    .attr("value");
+
+            if(csrf==null||csrf.isEmpty()){
+                throw new CfAuthException(
+                        "Failed to extract CSRF token from Codeforces login page"
+                );
+            }
+
+            Connection.Response loginResponse = Jsoup.connect(
+                    "https://codeforces.com/enter")
+                    .userAgent("Mozilla/5.0 (compatible; AlgoLens/1.0)")
+                    .data("handleOrEmail",handle)
+                    .data("password",password)
+                    .data("csrf_token",csrf)
+                    .data("action","enter")
+                    .cookies(loginPage.cookies())
+                    .method(Connection.Method.POST)
+                    .followRedirects(true)
+                    .execute();
+
+            if(loginResponse.url().toString().contains("/enter")){
+                throw new CfAuthException(
+                        "Codeforces login failed-check credentials"
+                );
+            }
+
+            Map<String,String> cookies = loginResponse.cookies();
+            if(!cookies.containsKey("JSESSIONID")){
+                throw new CfAuthException(
+                        "Codeforces login succeeded but no session cookie received"
+                );
+            }
+
+            sessionCookie = cookies.get("JSESSIONID");
+            lastLogin = LocalDateTime.now();
+
+        }catch (IOException e){
+          throw new CfAuthException(
+                  "Failed to connect to Codeforces:"+e.getMessage()
+          );
+        }
+
+    }
+}
